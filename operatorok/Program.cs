9d21b8fc96204f75630f9f977d95fc2ce7318dfb
bekesi142 <bekesibalint2006@gmail.com>
2023-05-24 19:14:16 +0200

    Add project files.

---
+++ operatorok/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+
+    List<string> lista = new List<string>();
+    File.ReadAllLines("kifejezesek.txt").ToList();
+    
+    string[] felbontottSor = sor.Split(" ");
+    int szam1 = int.Parse(felbontottSor[0]);
+    string jel = felbontottSor [1];
+    int szam2 = int.Parse(felbontottSor [2]);
+    
+
+
